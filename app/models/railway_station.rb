a8ac7526c761ddf02b9d15f515b60e45302160c3
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-03-22 14:02:21 +0500

    Исправлены замечания по заданию 12.1

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,9 +1,9 @@
 class RailwayStation < ActiveRecord::Base
-  validates :title, presence: true
   has_many :trains, foreign_key: :current_station_id
-  #has_and_belongs_to_many :routes
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :from_tickets, class_name: 'Ticket', foreign_key: :start_station_id
   has_many :to_tickets, class_name: 'Ticket', foreign_key: :end_station_id
+
+  validates :title, presence: true
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,7 @@
 class Route < ActiveRecord::Base
-  validates :name, presence: true
-
   has_many :railway_stations_routes
   has_many :stations, through: :railway_stations_routes
   has_many :trains
+
+  validates :name, presence: true
 end
