68b610aea482ec1018ed6e6af97ef0422b8086e0
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-03-24 14:23:17 +0500

    Исправлен и русифицирован интерфейс для создания/удаления/изменения поездов. Добавлена валидация в модель.

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,4 +2,6 @@ class Train < ActiveRecord::Base
   has_many :tickets
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :route
+
+  validates :number, presence: true
 end
