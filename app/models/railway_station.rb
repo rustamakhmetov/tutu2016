7c7bbf0746170c158081384f5f3767c120a053ce
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-03-28 13:19:22 +0500

    Добавлены методы для работы с порядковым номером станции. Сортировка станций маршрута по порядковому номеру.

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -6,4 +6,25 @@ class RailwayStation < ActiveRecord::Base
   has_many :to_tickets, class_name: 'Ticket', foreign_key: :end_station_id
 
   validates :title, presence: true
+
+  #scope :ordered, -> {joins(:railway_stations_routes).order('railway_stations_routes.position ASC')}
+  scope :ordered, -> {order('railway_stations_routes.position')}
+
+  def position(route)
+    obj = railway_stations_routes_object(route)
+    obj.position
+  end
+
+  def update_position(route, pos)
+    obj = railway_stations_routes_object(route)
+    obj.position = pos
+    obj.save
+  end
+
+  private
+
+  def railway_stations_routes_object(route)
+    self.railway_stations_routes.where(route_id: route).first
+  end
+
 end
