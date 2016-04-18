43dc5fc9022fc59cf6ce86e627dbb7424d5ff725
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-04-18 14:19:38 +0500

    Реализован ввод ФИО при регистрации пользователя

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
@@ -11,4 +12,13 @@ class ApplicationController < ActionController::Base
     end
     #current_user_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
+    #devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :admin, :email, :password, :password_confirmation, :current_password) }
+  end
+
 end
