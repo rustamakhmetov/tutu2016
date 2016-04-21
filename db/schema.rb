ef94f5f734384d1ff54c31576a65bf9b6c107e08
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-04-21 14:45:25 +0500

    Добавлены индексы

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417101156) do
+ActiveRecord::Schema.define(version: 20160421063731) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -24,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160417101156) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
+
+  add_index "carriages", ["id", "type"], name: "index_carriages_on_id_and_type", using: :btree
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -39,7 +44,7 @@ ActiveRecord::Schema.define(version: 20160417101156) do
     t.time    "departure_time"
   end
 
-  add_index "railway_stations_routes", ["position"], name: "index_railway_stations_routes_on_position"
+  add_index "railway_stations_routes", ["position"], name: "index_railway_stations_routes_on_position", using: :btree
 
   create_table "routes", force: :cascade do |t|
     t.string   "name"
@@ -67,6 +72,8 @@ ActiveRecord::Schema.define(version: 20160417101156) do
     t.boolean  "sorting_wagons",     default: false
   end
 
+  add_index "trains", ["current_station_id"], name: "index_trains_on_current_station_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",                             null: false
@@ -83,8 +90,8 @@ ActiveRecord::Schema.define(version: 20160417101156) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "wagons", force: :cascade do |t|
     t.string   "wagon_type"
@@ -100,7 +107,7 @@ ActiveRecord::Schema.define(version: 20160417101156) do
     t.integer  "number"
   end
 
-  add_index "wagons", ["number"], name: "index_wagons_on_number"
-  add_index "wagons", ["type"], name: "index_wagons_on_type"
+  add_index "wagons", ["number"], name: "index_wagons_on_number", using: :btree
+  add_index "wagons", ["type"], name: "index_wagons_on_type", using: :btree
 
 end
