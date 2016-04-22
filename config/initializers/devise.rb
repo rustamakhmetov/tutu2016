12c987d0a7792bca074c668ae635939a84d801dc
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2016-04-22 10:49:10 +0500

    БД изменена на PostgreSQL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'ed0e73ca6ecbdc051127bbd8a016b8660d530d221783c95e52aad87c98b59cd42a3333e8242335cc3eb5622b045e81ba4037b3018cd4fb722a742f8fdd3bcc81'
+  config.secret_key = 'ed0e73ca6ecbdc051127bbd8a016b8660d530d221783c95e52aad87c98b59cd42a3333e8242335cc3eb5622b045e81ba4037b3018cd4fb722a742f8fdd3bcc81'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
